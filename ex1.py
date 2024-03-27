1df28757e7c3fe889310c2e9894f973f5e609b3a
Andre <68014993+agapi-mante@users.noreply.github.com>
2024-03-27 16:01:49 +0500

    exercise for the first lecture
    Write a program that would ask
    username: First name, Last name, Age, Place of residence
    - last name, first name ("Your last name, first name?")
    - age (“How old are you?”)
    - place of residence (“Where do you live?”) After this
    would display three lines: “Your last name, first name”
    "Your age" "You live in"

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Запрашиваем у пользователя данные
+first_name = input("Ваше имя: ")
+last_name = input("Ваша фамилия: ")
+age = input("Сколько вам лет: ")
+location = input("Где вы живете: ")
+
+# Выводим данные пользователя
+print("Ваши фамилия, имя:", last_name, first_name)
+print("Ваш возраст:", age)
+print("Вы живете в", location)
